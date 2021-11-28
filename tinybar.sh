1053d1bad488eb664126203e3838938c94f90caa
phon31x <93177436+phon31x@users.noreply.github.com>
2021-11-28 11:41:13 +0530

    Create tinybar.sh

---
+++ tinybar.sh
@@ -0,0 +1,9 @@
+# script to Tiny-launch polybar
+
+#!/usr/bin/env bash
+
+echo "---" | tee -a /tmp/polybar2.log
+polybar tray (name of your bar here instead of tray) >> /tmp/polybar2.log 2>&1
+
+~                                                                                                    
+~   
