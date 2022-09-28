b493e26e77f13798c5826f77b9068ae9ad5db43e
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2022-09-28 15:22:50 +0100

    merged everyting into one python
    instead of closing with a mouse button and closing with an other , I made this script to do the same thing but in one click => better UX

---
+++ tray.py
@@ -0,0 +1,15 @@
+import subprocess
+#replace tray with ur tray bar name ([bar/'name'])or keep it tray   
+name = "tray"
+
+tray_PID = subprocess.run(
+	"xprop -name 'Polybar tray window' _NET_WM_PID | grep -o \'[[:digit:]]*'", 
+	shell=True,
+	capture_output=True)
+
+if tray_PID.returncode == 0:
+	subprocess.run(f'kill {tray_PID.stdout.decode()}',shell=True)
+else:
+	subprocess.run(f'polybar {name}', shell=True)
+
+#ubuntu ships with python , arch too , almost every distro out there
