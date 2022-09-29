6e6c7a13bcc8da070b553fb52d76a37705f724f4
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2022-09-29 20:12:17 +0100

    removed shell scripts

--- tinybar.sh
+++
@@ -1,9 +0,0 @@
-# script to Tiny-launch polybar
-
-#!/usr/bin/env bash
-
-echo "---" | tee -a /tmp/polybar2.log
-polybar tray (name of your bar here instead of tray) >> /tmp/polybar2.log 2>&1
-
-~                                                                                                    
-~   
